0b56a0430592984f2c02e90cf98efabf4d91b188
NoofSaeidh <noofsaeidh@outlook.com>
2018-01-13 20:53:39 +0300

    Add Tests for TableResolver

---
+++ tests/Core.Tests/Mocks/MockCellResolver.cs
@@ -0,0 +1,106 @@
+﻿// This file is licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using NSL.DataConversion.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSL.DataConversion.Core.Tests.Mocks
+{
+    public class MockCellResolver : ICellResolver
+    {
+        public virtual ICell<T> Resolve<T>(T value)
+        {
+            return new MockCell<T>(value);
+        }
+
+        public virtual ICell ResolveObject(object value)
+        {
+            if (value == null) return new MockCell(value);
+            return (ICell)Activator.CreateInstance(typeof(MockCell<>).MakeGenericType(value.GetType()), value);
+        }
+
+        public virtual ICell[,] ResolveToArray(object[,] value)
+        {
+            var imax = value.GetLength(0);
+            var jmax = value.GetLength(1);
+            var result = new ICell[imax, jmax];
+            for (int i = 0; i < imax; i++)
+            {
+                for (int j = 0; j < jmax; j++)
+                {
+                    result[i, j] = ResolveObject(value[i, j]);
+                }
+            }
+            return result;
+        }
+
+        public virtual ICell[,] ResolveToArray(IEnumerable<IEnumerable<object>> value)
+        {
+            var array = value.Select(x => x.ToArray()).ToArray();
+            var jmax = array.Max(x => x.Length);
+            var result = new ICell[array.Length, jmax];
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    result[i, j] = ResolveObject(array[i][j]);
+                }
+                for (int j = array[i].Length; j < jmax; j++)
+                {
+                    result[i, j] = ResolveObject(null);
+                }
+            }
+            return result;
+        }
+
+        public virtual IList<IList<ICell>> ResolveToList(object[,] value)
+        {
+            var imax = value.GetLength(0);
+            var jmax = value.GetLength(1);
+            var result = new List<IList<ICell>>(imax);
+            for (int i = 0; i < imax; i++)
+            {
+                result.Add(new List<ICell>(jmax));
+                for (int j = 0; j < jmax; j++)
+                {
+                    result[i].Add(ResolveObject(value[i, j]));
+                }
+            }
+            return result;
+        }
+
+        public virtual IList<IList<ICell>> ResolveToList(IEnumerable<IEnumerable<object>> value)
+        {
+            var result = value.Select(x => (IList<ICell>)x.Select(item => ResolveObject(item)).ToList()).ToList();
+            var max = result.Max(x => x.Count);
+            foreach (var item in result)
+            {
+                while (item.Count < max)
+                {
+                    item.Add(ResolveObject(null));
+                }
+            }
+            return result;
+        }
+
+        ICell<T> ICellResolver.Resolve<T>(T value) => Resolve(value);
+
+        ICell IResolver<object, ICell>.Resolve(object value) => Resolve(value);
+
+        ICell[,] IResolver<object[,], ICell[,]>.Resolve(object[,] value) => ResolveToArray(value);
+
+        ICell[,] IResolver<IEnumerable<IEnumerable<object>>, ICell[,]>.Resolve(IEnumerable<IEnumerable<object>> value) => ResolveToArray(value);
+
+        IList<IList<ICell>> IResolver<object[,], IList<IList<ICell>>>.Resolve(object[,] value) => ResolveToList(value);
+
+        IList<IList<ICell>> IResolver<IEnumerable<IEnumerable<object>>, IList<IList<ICell>>>.Resolve(IEnumerable<IEnumerable<object>> value) => ResolveToList(value);
+
+        ICell IGenericResolver<ICell>.ResolveGeneric<U>(U value) => Resolve(value);
+
+        ICell IObjectResolver<ICell>.ResolveObject(object value) => Resolve(value);
+    }
+}
---
+++ tests/Core.Tests/Unit/Common/TableResolverTests.cs
@@ -0,0 +1,105 @@
+﻿// This file is licensed under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using NSL.DataConversion.Core.Common;
+using NSL.DataConversion.Core.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NSL.DataConversion.Core.Tests.Unit.Common
+{
+    public class TableResolverTests
+    {
+        [Fact]
+        public void ResolveReadOnlyTable_Array_Works()
+        {
+            // Arrange
+            var table = new TableResolver(new MockCellResolver());
+            object[,] array =
+            {
+                { "string", 5 },
+                { true, null}
+            };
+            // Act
+            var result = table.ResolveReadOnlyTable(array);
+            // Assert
+            Assert.Equal(2, result.RowsCount);
+            Assert.Equal(2, result.ColumnsCount);
+            Assert.Collection(result
+                , item => Assert.Equal("string", item.Value)
+                , item => Assert.Equal(5, item.Value)
+                , item => Assert.Equal(true, item.Value)
+                , item => Assert.Equal(null, item.Value));
+        }
+
+        [Fact]
+        public void ResolveReadOnlyTable_Enumerable_Works()
+        {
+            // Arrange
+            var table = new TableResolver(new MockCellResolver());
+            var list = new List<List<object>>
+            {
+                new List<object> { "string", 5 },
+                new List<object> { true, null}
+            };
+            // Act
+            var result = table.ResolveReadOnlyTable(list);
+            // Assert
+            Assert.Equal(2, result.RowsCount);
+            Assert.Equal(2, result.ColumnsCount);
+            Assert.Collection(result
+                , item => Assert.Equal("string", item.Value)
+                , item => Assert.Equal(5, item.Value)
+                , item => Assert.Equal(true, item.Value)
+                , item => Assert.Equal(null, item.Value));
+        }
+
+        [Fact]
+        public void ResolveModifiableTable_Array_Works()
+        {
+            // Arrange
+            var table = new TableResolver(new MockCellResolver());
+            object[,] array =
+            {
+                { "string", 5 },
+                { true, null}
+            };
+            // Act
+            var result = table.ResolveModifiableTable(array);
+            // Assert
+            Assert.Equal(2, result.RowsCount);
+            Assert.Equal(2, result.ColumnsCount);
+            Assert.Collection(result
+                , item => Assert.Equal("string", item.Value)
+                , item => Assert.Equal(5, item.Value)
+                , item => Assert.Equal(true, item.Value)
+                , item => Assert.Equal(null, item.Value));
+        }
+
+        [Fact]
+        public void ResolveModifiableTable_Enumerable_Works()
+        {
+            // Arrange
+            var table = new TableResolver(new MockCellResolver());
+            var list = new List<List<object>>
+            {
+                new List<object> { "string", 5 },
+                new List<object> { true, null}
+            };
+            // Act
+            var result = table.ResolveModifiableTable(list);
+            // Assert
+            Assert.Equal(2, result.RowsCount);
+            Assert.Equal(2, result.ColumnsCount);
+            Assert.Collection(result
+                , item => Assert.Equal("string", item.Value)
+                , item => Assert.Equal(5, item.Value)
+                , item => Assert.Equal(true, item.Value)
+                , item => Assert.Equal(null, item.Value));
+        }
+    }
+}
